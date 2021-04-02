84e237d71087bffc8e6e21b8a6bb5fae10fa12f3
Adam Toner <adam@192.168.1.108>
2021-04-02 14:40:06 +0100

    Finished 1.2

--- src/prt1.py
+++ src/prt1.py
@@ -9,9 +9,9 @@
 import os
 import time
 import numpy as np
-from numpy.lib.arraysetops import unique
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+import collections
 
 start = time.time() #start the timer
 
@@ -29,20 +29,25 @@ tdf = pd.read_csv( DATA_DIR + DATA_FILE, encoding = "ISO-8859-1" )
 # Create a function to print the checkpoint times to see where the program can be improved
 def time_check( cp ):
     if TIMING:
-        print( f'\n## {cp}: { time.time() - start }' )
+        print( f'\n## { cp }: { time.time() - start }' )
+
+def cp_time( id ):
+    if TIMING:
+        print( f'\n## checkpoint: { time.time() - start }' )
+    return time.time()
 
 ######################## SECTION 1.1 ########################
 time_check( 'Start 1.1' )
 
-
-
 # Print the number of unique sentiments.
-print( f'There are {} possible sentiments that a tweet may have.' )
+print( f'There are { len( tdf.Sentiment.value_counts() ) } possible sentiments that a tweet may have.' )
 # Find the second most popular sentiment in the tweets.
-print( f'The second most popular tweet sentiment is {  }.' )
+print( f'The second most popular tweet sentiment is { tdf.Sentiment.value_counts().index[1] }.' )
 # Find the date with the greates number of extremely positive tweets.
-print( f'The day with the most extremely positive tweets was {  }.' )
+print( f'The day with the most extremely positive tweets was { tdf[tdf[ "Sentiment" ] == "Extremely Positive"].TweetAt.value_counts().index[0] }.' )
 
+tdf[ 'Tweet' ] = tdf[ 'OriginalTweet' ].str.lower()
+tdf[ 'Tweet' ] = tdf[ 'Tweet' ].str.replace( '[^a-zA-Z]', ' ' )
 
 # Using vectorized string methods to clean up the spacing
 def clean_spacing( series ):
@@ -50,22 +55,32 @@ def clean_spacing( series ):
     series = series.str.strip() #strip whitespace before tokenization to avoid counting whitespace as words
     return series
 
+tdf[ 'Tweet' ] = clean_spacing( tdf[ 'Tweet' ])
+
 if DEBUGGING: 
     print( '\n\n Original Tweets:' )
-    print( )
+    print( tdf[ 'OriginalTweet' ])
     print( '\n\n Altered Tweets:' )
-    print( )
+    print( tdf[ 'Tweet' ])
 
 ######################## SECTION 1.2 ########################
 # Tokenize the tweets
 time_check( 'Start 1.2' )
 
+checkpoint = time.time()
+tdf[ 'Tokens' ] = tdf.Tweet.str.split()
+checkpoint = cp_time( checkpoint )
+corpus = pd.Series( ' '.join(tdf[ 'Tweet' ]).split() )
+checkpoint = cp_time( checkpoint )
+
 # Print the number of words in the corpus
-print( f'With stop words, there are {} words total, {} of which are unique.' )
-print( f'Without stop words, there are {} words total, {} of which are unique.' )
-print( '\nThe ten most frequently used words are:')
-print( f'With stop words: {}' )
-print( f'Without stop words: {}' )
+print( f'\nWith stop words, there are { len( corpus ) } words total, { len( corpus.unique() ) } of which are unique.' )
+print( f'The ten most frequently used words are: { ", ".join(corpus.value_counts()[:10].index.tolist()) }')
+
+# Remove the stop words from the corpus
+corpus = corpus[(corpus.str.len() > 2)]
+print( f'\nWithout stop words, there are { len( corpus.unique() ) } words total, { len( corpus.unique() ) } of which are unique.' )
+print( f'The ten most frequently used (non-stop) words are: { ", ".join(corpus.value_counts()[:10].index.tolist()) }')
 
 ######################## SECTION 1.3 ########################
 time_check( 'Start 1.3' )
